f2b5f00dad023a246902a7dccf8dd141a2f55ea8
dm0114 <68996680+dm0114@users.noreply.github.com>
2022-09-21 19:09:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.89 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,4 +1,3 @@
-
 function solution(board, moves) {
   let answer = 0
   const bucket = []
@@ -6,9 +5,7 @@ function solution(board, moves) {
   const X = board[0].length
   const NewBoard = Array.from(Array(Y), () => new Array())
 
-  moves = moves.map((el) => --el)
-
-
+  // board를 돌려서 0이 아닌 값만 큐로 추출
   for (let x = 0; x < X; x++) {
     for (let y = 0; y < Y; y++) {
       if (board[y][x] !== 0) {
@@ -17,6 +14,8 @@ function solution(board, moves) {
     }
   }
   
+  // moves를 배열의 인덱스에 맞추고 shift로 bucket에 담기
+  moves = moves.map((el) => --el)
   let tmp;
   for (el of moves) {
     tmp = NewBoard[el].shift()
@@ -25,9 +24,8 @@ function solution(board, moves) {
     }
   }
 
-
+  // 같은 값 터뜨리기
   let i = 0
-
   while (i < bucket.length-1) {
     if (bucket[i] === bucket[i+1]) {
       bucket.splice(i, 2)
