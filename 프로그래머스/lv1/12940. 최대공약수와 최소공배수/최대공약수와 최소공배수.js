77543ef077ff3eae944b8d80abdbe7873870db6d
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-19 15:32:45 +0900

    Update 최대공약수와 최소공배수.js

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,3 +1,5 @@
+// https://velog.io/@devjade/JavaScript%EB%A1%9C-%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98GCD-%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98LCM-%EA%B5%AC%ED%95%98%EA%B8%B0
+
 function solution(num1, num2) {
   const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
   const lcm = (a, b) => a * b / gcd(a, b);
