af3feee4b51aca0495e271b382d50d4cffcc695c
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-19 13:34:47 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,3 +1,10 @@
 function solution(s) {
   return (s.length === 4 || s.length === 6) && s.split('').every(v => !isNaN(v))
 }
+
+/*
+function alpha_string46(s){
+  var regex = /^\d{6}$|^\d{4}$/;
+  return regex.test(s);
+}
+*/
