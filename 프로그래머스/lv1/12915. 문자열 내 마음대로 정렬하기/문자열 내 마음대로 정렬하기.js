f83180d12055d43029b813a7d4ddfe5c89650bfd
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-19 20:24:51 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -5,3 +5,9 @@ function solution(strings, n) {
     else return -1
   })
 }
+
+/*
+function solution(strings, n) {
+    return strings.sort((s1, s2) => s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n]));
+}
+*/
