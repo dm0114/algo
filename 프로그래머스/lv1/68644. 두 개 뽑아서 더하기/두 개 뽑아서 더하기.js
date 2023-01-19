59ad807313d6b8efb3cdd023c005e29d1144e052
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-19 22:04:09 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  let answer = new Set();
+
+  const dfs = (lv, st, sum) => {
+    if (lv === 2) return  answer.add(sum)
+    for (let i = st; i < numbers.length; i++) {
+      dfs(lv+1, i+1, sum+numbers[i])
+    }
+  }
+  dfs(0, 0, 0)
+
+  return [...answer].sort((a, b) => a - b);
+}
