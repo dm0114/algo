a339fc75f29872f62a12cedeb557e82afe12b488
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-20 21:40:42 +0900

    [level 1] Title: 소수 찾기, Time: 53.18 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+
+  return arr.reduce((p, v) => p + (v ? 1 : 0));
+}
