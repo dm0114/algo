d32ca6d53b28bb1b392a02850c6399064d6f0e26
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-20 22:01:18 +0900

    [level 1] Title: 소수 만들기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,10 +1,9 @@
 function solution(nums) {
   let ans = 0
-  const n = 3000
-  let arr = Array(n + 1)
-    .fill(true)
-    .fill(false, 0, 2);
 
+  // 소수 구해놓기, 최대 1000 -> 세 자리를 더한 MAX 값은 2997
+  const n = 2997
+  let arr = Array(n + 1).fill(true).fill(false, 0, 2);
   for (let i = 2; i * i <= n; i++) {
     if (arr[i]) {
       for (let j = i * i; j <= n; j += i) {
@@ -13,6 +12,7 @@ function solution(nums) {
     }
   }
 
+  // dfs로 순회
   const dfs = (lv, sum, st) => {
     if (lv === 3) return arr[sum] ? ans += 1 : null
 
@@ -20,7 +20,7 @@ function solution(nums) {
       dfs(lv+1, sum+nums[i], i+1)
     }
   }
+  
   dfs(0, 0, 0)
-
   return ans
 }
