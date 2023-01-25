85027c9b431d15b3b3a87e178b3823db6c93840d
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-25 19:39:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 159.76 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,14 +1,13 @@
 function solution(elements) {
   const LEN = elements.length;
   const dict = {};
-  
-  elements.forEach((_, i) => {
-    dict[
-      elements.reduce((p, _, j) => {
-        dict[p] || (() => {dict[p] = true})();
-        return p + elements[(i + j) % LEN];
-      }, 0)
-    ] = true;
+
+  elements.forEach((_, i) => {    
+    
+    dict[elements.reduce((p, _, j) => {
+      dict[p] || (() => {dict[p] = true})();
+      return p + elements[(i + j) % LEN];
+      }, 0)] = true;
   });
 
   return Object.keys(dict).length - 1;
