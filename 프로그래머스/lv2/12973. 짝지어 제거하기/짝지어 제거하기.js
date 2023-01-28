334e770eae6a40988f6f2ff490ad54975d8d45c4
Oddm <68996680+dm0114@users.noreply.github.com>
2023-01-29 01:57:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 55.13 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,7 +2,7 @@ function solution(s) {
   const stack = []
   s.split("").forEach(v => {
     stack.push(v)
-    
+
     while (
       stack.length >= 2 &&
       stack[stack.length - 1] === stack[stack.length - 2]
