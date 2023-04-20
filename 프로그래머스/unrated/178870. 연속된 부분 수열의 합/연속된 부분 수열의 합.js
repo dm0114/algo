c55b9faa19270754a8782113ac035429886ad6c8
Oddm <68996680+dm0114@users.noreply.github.com>
2023-04-20 21:55:31 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,45 +1,32 @@
-class Queue {
-  constructor(k) {
-    this.front = 0;
-    this.rear = 0;
-    this.sum = 0;
-    this.k = k;
-  }
-
-  enqueue(value) {
-    this.rear++
-    this.sum += value;
-  }
-
-  dequeue(seq) {
-    this.sum -= seq[this.front]
-    this.front += 1;
-  }
-
-  size() {
-    return this.rear - this.front;
-  }
-
-  isLower() {
-    return this.k > this.sum
-  }
-
-  isUpper() {
-    return this.k < this.sum
-  }
-
-  isSame() {
-    return this.k === this.sum
+const queue = ( init ) => {
+  let front = 0, rear = 0, sum = 0, k = init
+  
+  return {
+    enqueue: (value) => {
+      rear++
+      sum += value
+    },
+  
+    dequeue: (seq) => {
+      sum -= seq[front]
+      front += 1;
+    },
+  
+    size: () => rear - front,
+    isLower: () => k > sum,
+    isUpper: () => k < sum,
+    isSame: () => k === sum,
+    answer: () => [front, rear - 1]
   }
 }
 
 function solution(sequence, k) {
   let answer = []
   let len = Infinity
-  const q = new Queue(k)
+  const q = queue(k)
 
   for (const key in sequence) {
-    q.enqueue(sequence[key], +key)      
+    q.enqueue(sequence[key])
     
     while (q.isUpper()) {
       q.dequeue(sequence)
@@ -48,7 +35,7 @@ function solution(sequence, k) {
     if (q.isSame()) {
       if (len > q.size()) { 
         len = q.size()
-        answer = [q.front, q.rear - 1]
+        answer = q.answer()
       }
     }
   }
